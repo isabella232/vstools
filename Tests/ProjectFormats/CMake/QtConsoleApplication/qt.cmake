473fdb41ddc5626729cdda360ab418679d0276b7
Ali Can Demiralp <ali.demiralp@qt.io>
2024-10-18 14:26:04 +0200

    Use qt_add_resources instead of CMAKE_AUTORCC in templates and tests
    Fixes: QTVSADDINBUG-1236
    Change-Id: I5dd104867154e89f0377f51263dcf00293b05c80
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- Tests/ProjectFormats/CMake/QtConsoleApplication/qt.cmake
+++ Tests/ProjectFormats/CMake/QtConsoleApplication/qt.cmake
@@ -1,7 +1,6 @@
 if(QT_VERSION VERSION_LESS 6.3)
     macro(qt_standard_project_setup)
         set(CMAKE_AUTOMOC ON)
-        set(CMAKE_AUTORCC ON)
         set(CMAKE_AUTOUIC ON)
     endmacro()
 endif()
