bfac24d897e4af9a9193fd358c1798f24056c301
Miguel Costa <miguel.costa@qt.io>
2024-11-14 23:25:29 +0100

    Add auto-test for static QML plugin
    Task-number: QTVSADDINBUG-1173
    Change-Id: I4be14dd47dd17d5a7b54030705d657a98bca328f
    Reviewed-by: Karsten Heimrich <karsten.heimrich@qt.io>

--- Tests/Test_QtMsBuild.Build/Test_QmlStatic.cs
+++ Tests/Test_QtMsBuild.Build/Test_QmlStatic.cs
@@ -3,9 +3,13 @@
  SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 ***************************************************************************************************/
 
+using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using Microsoft.Build.Construction;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32;
 
 namespace QtVsTools.Test.QtMsBuild.Build
 {
@@ -72,6 +76,58 @@ import Bar;
 QmlObject {{ }}
 ";
             Assert.IsTrue(resultFile == expectedFile);
+        }
+
+        [TestMethod]
+        public void QtQmlStaticPlugin()
+        {
+            using var qtVersions = Registry.CurrentUser
+                .OpenSubKey(@"SOFTWARE\QtProject\QtVsTools\Versions");
+            if (!qtVersions.GetSubKeyNames().Contains("dev_static"))
+                Assert.Inconclusive("Requires static build registered as 'dev_static'.");
+
+            using var temp = new TempProject();
+            temp.Clone(Path.Combine(Properties.SolutionDir,
+                @"Tests\ProjectTemplates\QtQuickApplication", "QtQuickApplication.vcxproj"));
+
+            var xml = ProjectRootElement.Open(temp.ProjectPath);
+            foreach (var qtSettings in xml.PropertyGroups.Where(x => x.Label == "QtSettings")) {
+                qtSettings.SetProperty("QtInstall", "dev_static");
+                qtSettings.SetProperty("QtQMLDebugEnable", "false");
+            }
+            xml.Save();
+
+            File.WriteAllText($@"{temp.ProjectDir}\main.qml", @"
+import QtQuick 2.9
+import QtQuick.Window 2.2
+import QtQuick.Particles 2.0
+Item { Component.onCompleted: Qt.exit(42) }
+");
+            var project = MsBuild.Evaluate(temp.ProjectPath,
+                ("Platform", "x64"), ("Configuration", "Debug"));
+            var build = MsBuild.Prepare(project);
+            Assert.IsTrue(MsBuild.Run(build));
+
+            Assert.IsTrue(File.Exists(project.ExpandString("$(TargetPath)")));
+            Assert.IsTrue(File.Exists(Path.Combine(
+                project.ExpandString("$(QtVarsOutputDir)"), "qtvars_qml_plugin_import.cpp")));
+            Assert.IsTrue(build.Project
+                .GetItems("ClCompile")
+                .Select(x => x.GetMetadataValue("Filename"))
+                .Contains("qtvars_qml_plugin_import"));
+
+            var proc = Process.Start(new ProcessStartInfo
+            {
+                FileName = project.ExpandString("$(TargetPath)"),
+                WorkingDirectory = project.ExpandString("$(OutDir)"),
+                CreateNoWindow = true,
+                UseShellExecute = false
+            });
+            if (!proc.WaitForExit(3000)) {
+                proc.Kill();
+                Assert.Fail();
+            }
+            Assert.IsTrue(proc.ExitCode == 42);
         }
     }
 }
